83c980d75ea553d10859de561f29bada884b08ea
Fernando <nandosbx@users.noreply.github.com>
2021-11-27 22:07:44 -0300

    Expo Google Fonts

--- gameplay/App.tsx
+++ gameplay/App.tsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { View, Text } from "react-native";
 import { SignIn } from "./src/screens/SignIn";
+import AppLoading from "expo-app-loading";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 
 export default function App() {
-  return <SignIn />;
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ fontFamily: "Inter_900Black", fontSize: 40 }}>
+          Inter Black
+        </Text>
+      </View>
+    );
+  }
 }
