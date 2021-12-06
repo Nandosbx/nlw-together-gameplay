ef85c5593e6a7e751d025e99c55918bc237cdbea
Fernando <nandosbx@users.noreply.github.com>
2021-12-05 23:22:58 -0300

    Expo Linear Gradient

--- gameplay/src/components/Background/index.tsx
+++ gameplay/src/components/Background/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { LinearGradient } from "expo-linear-gradient";
 import {
   View,
   Text,
@@ -10,20 +10,6 @@ import {
 
 import { styles } from "./styles";
 
-import DiscordImg from "../../assets/discord.png";
-
-type Props = TouchableOpacityProps & {
-  title: string;
-};
-
-export function ButtonIcon({ title, ...rest }: Props) {
-  return (
-    <TouchableOpacity style={styles.container} {...rest}>
-      <View style={styles.iconWrapper}>
-        <Image source={DiscordImg} style={styles.icon} />
-      </View>
-
-      <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
-  );
+export function Background() {
+  return <LinearGradient></LinearGradient>;
 }
