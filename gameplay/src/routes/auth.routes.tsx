ac35afb845c6119908ebe84dda6b504b35842aba
Fernando <nandosbx@users.noreply.github.com>
2021-12-11 00:04:05 -0300

    Navigator

---
+++ gameplay/src/routes/auth.routes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { View } from "react-native";
+
+import { Home } from "../screens/Home";
+import { SignIn } from "../screens/SignIn";
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AuthRoutes() {
+  return (
+    <Navigator>
+      <Screen name="SignIn" componenet={SignIn} />
+      <Screen name="Home" componenet={Home} />
+    </Navigator>
+  );
+}
