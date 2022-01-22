0e341cf2ff28029cc5b12bab5208478a15e9ffd7
Fernando <nandosbx@users.noreply.github.com>
2022-01-21 23:52:44 -0300

    Update

--- gameplay/src/components/Category/index.tsx
+++ gameplay/src/components/Category/index.tsx
@@ -1,9 +1,41 @@
 import React from "react";
 
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import { SvgProps } from "react-native-svg";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 
+import { theme } from "../../global/styles/themes";
 import { styles } from "./styles";
+import { LinearGradient } from "expo-linear-gradient";
 
-export function Category() {
-  return <View></View>;
+type Props = RectButtonProps & {
+  title: string;
+  icon: React.FC<SvgProps>;
+  checked?: boolean;
+};
+
+export function Category({
+  title,
+  icon: Icon,
+  checked = false,
+  ...rest
+}: Props) {
+  const { secondary50, secondary70 } = theme.colors;
+
+  return (
+    <RectButton {...rest}>
+      <LinearGradient
+        colors={[secondary50, secondary70]}
+        style={styles.container}
+      >
+        <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
+          <View style={checked ? styles.checked : styles.check}>
+            <Icon width={48} height={48} />
+          </View>
+
+          <Text style={styles.title}></Text>
+        </View>
+      </LinearGradient>
+    </RectButton>
+  );
 }
