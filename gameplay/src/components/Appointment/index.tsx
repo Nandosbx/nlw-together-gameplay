62350adc0397e50863f4c58033eb1eee64dff56a
Fernando <nandosbx@users.noreply.github.com>
2022-01-30 01:06:37 -0300

    GuildItem

--- gameplay/src/components/Appointment/index.tsx
+++ gameplay/src/components/Appointment/index.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 
-import {
-  View
-} from 'react-native'
+import { View } from "react-native";
 
-import { styles } from './styles'
+import { styles } from "./styles";
 
-export function Appointment(){
+export type GuildProps = {};
+
+export type AppointmentProps = {
+  id: string;
+  guild: GuildProps;
+  category: string;
+  date: string;
+  description: string;
+};
+
+type Props = RectButtonProps & {
+  data: AppointmentProps;
+};
+
+export function Appointment({ data, ...rest }: Props) {
   return (
-    <View>
-
-    </View>
-  )
+    <RectButton {...rest}>
+      <View style={styles.container}>
+        <GuildItem />
+      </View>
+    </RectButton>
+  );
 }
