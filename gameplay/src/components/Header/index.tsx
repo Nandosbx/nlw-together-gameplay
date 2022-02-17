5dbbd47e7f0cc8bd668f37b218148faa7b60fd85
Fernando <nandosbx@users.noreply.github.com>
2022-02-16 22:46:44 -0300

    Header Props

--- gameplay/src/components/Header/index.tsx
+++ gameplay/src/components/Header/index.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-
+import React, { ReactNode } from "react";
+import { LinearGradient } from "@expo-linear-gradient";
 import { View } from "react-native";
 
 import { styles } from "./styles";
+import { theme } from "../../global/styles/themes";
 
-export function Header() {
-  return <View></View>;
+type Props = {
+  title: string;
+  action?: ReactNode;
+};
+
+export function Header({ title, action }: Props) {
+  const { secondary40, secondary100 } = theme.colors;
+
+  return <LinearGradient colors={[secondary40, secondary100]}></LinearGradient>;
 }
